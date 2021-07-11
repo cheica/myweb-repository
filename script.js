99cf9fc82d26c514858d7e86dfba71c5b4a4d6d4
Chelsea <chelsea.icaza10@hotmail.com>
2021-07-11 13:29:10 -0400

    Introduction

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const h2 = document.createElement('h2');
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Brief Description:";
 
 document.querySelector('body').appendChild(h2); 
 
+let p = document.createElement("p");
+document.querySelector('h2').appendChild(p);
+
+p.innerHTML = "Welcome to my website! This page includes some basic information about me. Hope you enjoy!";
